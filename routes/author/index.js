97d4be81a74b5887b994855fb6ea478564b48f99
Ina Nuri Yohari <inanuriyohari@gmail.com>
2020-02-17 16:16:58 +0700

    blog crud fixed

--- routes/author/index.js
+++ routes/author/index.js
@@ -2,10 +2,10 @@ const express = require("express")
 const router = express.Router()
 
 router.get("/", require("./controller").getAll);
-// router.get("/:email", require("./controller").getByEmail);
-// router.get("/:id", require("./controller").getById);
-// router.post("/", require("./controller").addData);
-// router.put("/:id", require("./controller").updateOne);
-// router.delete("/:id", require("./controller").deleteOne);
+router.get("/email/:email", require("./controller").getByEmail);
+router.get("/id/:id", require("./controller").getByID);
+router.post("/", require("./controller").addData);
+router.put("/id/:id", require("./controller").updateOne);
+router.delete("/id/:id", require("./controller").deleteOne);
 
 module.exports = router;
--- routes/blogs/controller.js
+++ routes/blogs/controller.js
@@ -18,125 +18,105 @@ module.exports = {
     } catch (error) {
       console.log(error);
     }
+  },
+
+  getByID: (req, res) => {
+    try {
+      const { id } = req.params;
+      connection.query(`SELECT * FROM Blogs WHERE Blogs.ID = ${id}`, (error, results, fields) => {
+        if (error) {
+          res
+            .status(500)
+            .send({ message: `There's some problem: ${error.sqlMessage}` })
+        } else {
+          res.status(200)
+            .send({
+              message: `Show data by id ${id}`, data: results
+            })
+        }
+      });
+    } catch (error) {
+      console.log(error)
+    }
+  },
+
+  getByEmail: (req, res) => {
+    try {
+      const { email } = req.params;
+      connection.query(`SELECT Blogs.ID, Blogs.Title, Blogs.Picture, Blogs. Description, Blogs.Date, Blogs.AuthorID FROM Blogs INNER JOIN Author ON Blogs.AuthorID = Author.ID WHERE Author.Email = "${email}"`, (
+        error,
+        results,
+        fields
+      ) => {
+        if (error) {
+          console.log(error);
+        } else {
+          res.status(200).send({
+            message: `Show User by email ${email}`, data: results
+          });
+        };
+      });
+    } catch (error) {
+      console.log(error)
+    }
+  },
+
+  addData: (req, res) => {
+    try {
+      connection.query("INSERT INTO Blogs SET ?", req.body, function(
+        error,
+        results,
+        fields
+      ) {
+        if (error) {
+          console.log(error);
+        } else {
+          res.status(200).send({
+            message: "New Blog is successfully added", data: results
+          });
+        }
+      });
+    } catch (error) {
+        console.log(error)
+    }
+  },
+
+  updateOne: (req,res) => {
+    try {
+        const {id} = req.params;
+        connection.query(`UPDATE Blogs SET ? WHERE ID = ${id}`, req.body, function(
+          error,
+          results,
+          fields
+        ) {
+          if (error) {
+            console.log(error);
+          } else {
+            res.status(200).send({
+              message: `Update Blog with id ${id} is succeeded`, data: results
+            });
+          }
+        });
+      } catch (error) {
+          console.log(error)
+      }
+  },
+
+  deleteOne: (req, res) => {
+    try {
+      const {id} = req.params;
+      connection.query(`DELETE from Blogs WHERE id = ${id}`, function(error, results, fields) {
+        if (error) {
+          console.log(error);
+        } else {
+          res.status(200).send({
+            message: `Blog with id ${id} is deleted`
+          })
+        }
+      })
+    } catch (error) {
+      console.log(error)
+    }
   }
-
-  // getByID: (req, res) => {
-  //   try {
-  //     const { id } = req.param;
-  //     connection.query(`SELECT * FROM Blogs WHERE ID = ${id}`, (error, results, fields) => {
-  //       if (error) {
-  //         res
-  //           .status(500)
-  //           .send({ message: `There's some problem: ${error.sqlMessage}` })
-  //       } else {
-  //         res.status(200)
-  //           .send({
-  //             message: `Show data by id ${id}`, data: results
-  //           })
-  //       }
-  //     });
-  //   } catch (error) {
-  //     console.log(error)
-  //   }
-  // }
-
-  // getByEmail: (req, res) => {
-  //   try {
-  //     const { email } = req.params;
-  //     connection.query(`SELECT * Blogs FROM Blogs LEFT JOIN Users ON Blogs.AuthorID = Users.ID WHERE Users.Email = ${email}`, (
-  //       error,
-  //       results,
-  //       fields
-  //     ) => {
-  //       if (error) {
-  //         console.log(error);
-  //       } else {
-  //         res.status(200).send({
-  //           message: `Show User by email ${email}`, data: results
-  //         });
-  //       };
-  //     });
-  //   } catch (error) {
-  //     console.log(error)
-  //   }
-  // }
-
-  // getOne: (req, res) => {
-  //   try {
-  //     const {id} = req.params;
-  //     connection.query(`SELECT Blogs.id, Blogs.Title, Blogs.description Blogs.Author FROM Blogs INNER JOIN Users ON Blogs.Author=Users.Name WHERE Blogs.Name = ${id}`, (error, results, fields) => {
-  //       if (error) {
-  //         res
-  //           .status(500)
-  //           .send({ message: `there is some problems: ${error.sqlMessage}` });
-  //       } else {
-  //         res.status(200).send({
-  //           message: "show all data tokoh",
-  //           data: results
-  //         });
-  //       }
-  //     });
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // },
-
-  // addData: (req, res) => {
-  //   try {
-  //     connection.query("INSERT INTO Blogs SET ?", req.body, function(
-  //       error,
-  //       results,
-  //       fields
-  //     ) {
-  //       if (error) {
-  //         console.log(error);
-  //       } else {
-  //         res.status(200).send({
-  //           message: "New Blog is successfully added", data: results
-  //         });
-  //       }
-  //     });
-  //   } catch (error) {
-  //       console.log(error)
-  //   }
-  // },
-
-  // updateOne: (req,res) => {
-  //   try {
-  //       const {id} = req.params;
-  //       connection.query(`UPDATE Blogs SET ? WHERE id=?`, req.body, id, function(
-  //         error,
-  //         results,
-  //         fields
-  //       ) {
-  //         if (error) {
-  //           console.log(error);
-  //         } else {
-  //           res.status(200).send({
-  //             message: `Update Blogs with id ${id} is succeeded`, data: results
-  //           });
-  //         }
-  //       });
-  //     } catch (error) {
-  //         console.log(error)
-  //     }
-  // },
-
-  // deleteOne: (req, res) => {
-  //   try {
-  //     const {id} = req.params;
-  //     connection.query(`DELETE from Blogs WHERE id = ${id}`, function(error, results, fields) {
-  //       if (error) {
-  //         console.log(error);
-  //       } else {
-  //         res.status(200).send({
-  //           message: `Delete Blog with id ${id} is succeeded`
-  //         })
-  //       }
-  //     })
-  //   } catch (error) {
-  //     console.log(error)
-  //   }
-  // },
 
 };
